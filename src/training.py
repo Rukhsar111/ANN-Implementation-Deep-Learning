da337cf6a2611e5f5a23edfac8556137588e1e33
RukhsarAhmed <rukhsar101095@gmail.com>
2021-10-15 21:36:57 +0530

    added save model function

--- src/training.py
+++ src/training.py
@@ -1,7 +1,9 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model , save_model
 import argparse
+import os
+
 
 def training(config_path):
     config = read_config(config_path)
@@ -23,6 +25,19 @@ def training(config_path):
     VALIDATION = (X_valid, y_valid)
 
     history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+
+    model_dir_path= os.path.join(artifacts_dir  , model_dir)
+    os.makedirs(model_dir_path , exist_ok=True)
+
+
+    model_name = config["artifacts"]["model_name"]
+    save_model(model, model_name , model_dir_path)
+
+
+
 
     
     
